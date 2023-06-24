18e5d7e027b6e195f4c15b69dbb589421a4bf6f8
Joshua Krinsky <44280379+Joshpk29@users.noreply.github.com>
2023-06-23 22:52:04 -0400

    Update read_files.py

--- analysis_scripts/read_files.py
+++ analysis_scripts/read_files.py
@@ -7,7 +7,7 @@ import random
 sal_metric_name = "mean_recall"
 thresh_metric_name = "and_or_threshold"
 
-for data in ['imd2020', 'MFC18_small']:
+for data in ['imd2020', 'MFC18_small', 'korus']:
     for partition in ['under_two', 'two_to_four', 'four_to_six', 'six_to_eight', 'above_eight']:
         values = np.load(f'./data_splits/5_split/{data}_{sal_metric_name}_{partition}.npy', allow_pickle = True)
         picker = random.randrange(len(values))
