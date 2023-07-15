053206372d5288daac95b77fc07718b00eca5722
Joshua Krinsky <44280379+Joshpk29@users.noreply.github.com>
2023-07-15 16:14:46 -0400

    Update find_lengths.py
    Update to print out lengths of each partition -- used for getting lengths to put into boxplot.py

--- analysis_scripts/find_lengths.py
+++ analysis_scripts/find_lengths.py
@@ -7,8 +7,8 @@ import random
 sal_metric_name = "mean_recall"
 thresh_metric_name = "and_or_threshold"
 
-for data in ['imd2020', 'MFC18_small', 'korus']:
+for data in ['korus','MFC18_small', 'imd2020', 'imd2020_resize', 'MFC18_resize', 'korus_size_up', 'korus_SE', 'MFC18_SE','imd2020_SE']:
     for partition in ['under_two', 'two_to_four', 'four_to_six', 'six_to_eight', 'above_eight']:
-        values = np.load(f'./data_splits/5_split/{data}_{sal_metric_name}_{partition}.npy', allow_pickle = True)
-        picker = random.randrange(len(values))
-        print(f'in dataset: {data} for partition: {partition} -- {values[picker]}')
+        values = np.load(f'./WIFS_ready/sal_groups/{data}_{sal_metric_name}_{thresh_metric_name}_{partition}.npy', allow_pickle = True)
+        length = len(values)
+        print(f'in dataset: {data} for partition: {partition} -- {length}')
