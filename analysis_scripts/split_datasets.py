4392ccee9d54163d9df51ab39959e9f0196ac132
Joshua Krinsky <44280379+Joshpk29@users.noreply.github.com>
2023-07-15 16:18:32 -0400

    Update split_datasets.py

--- analysis_scripts/split_datasets.py
+++ analysis_scripts/split_datasets.py
@@ -35,77 +35,82 @@ sal_metric_display = "mean recall"
 split_metric_name = "and_or_threshold"
 
 #all datasets to run
-datasets = ['korus', 'korus_size_up','korus_SE','MFC18_small','MFC18_resize', 'MFC18_SE', 'imd2020', 'imd2020_resize', 'imd2020_SE']
+sums = []
+lens = []
+
+datasets = ['korus', 'MFC18_small', 'imd2020']
 for i in datasets:
 
     #name formatting for graphs
     dataset_display = i
     if(i == 'korus_SE'):
-        dataset_display = 'Korus Saliency Enhanced'
+        dataset_display = 'Realistic Tampering Saliency Enhanced'
     if(i == 'korus'):
-        dataset_display = 'Korus'
+        dataset_display = 'Realistic Tampering'
     if(i == 'korus_size_up'):
-        dataset_display = 'Korus Resized'
-
-
-    if(i == 'MFC18_small'):
-        dataset_display = 'MFC18'
-    if(i == 'MFC18_SE'):
-        dataset_display = 'MFC18 Saliency Enhanced'
-    if(i == 'MFC18_resize'):
-        dataset_display = 'MFC18 Resized'
-
-    if(i == 'imd2020'):
-        dataset_display = 'IMD2020'
-    if(i == 'imd2020_SE'):
-        dataset_display = 'IMD2020 Saliency Enhanced'
-    if(i == 'imd2020_resize'):
-        dataset_display = 'IMD2020 Resized'
+        dataset_display = 'Realistic Tampering Resized'
 
     #load scores
-    scores = load_numpy(f'scores/combine_maps_{sal_metric_name}_{i}.npy')
+    scores = load_numpy(f'./WIFS_ready/saliency_scores/combine_maps_{sal_metric_name}_{i}.npy')
 
     #human split -- only on korus dataset
     if(i == 'korus' or i == 'korus_SE'):
-        sal_scores = load_numpy(f'scores/human_study_sal_{sal_metric_name}_{i}.npy')
+        sal_scores = load_numpy(f'./WIFS_ready/saliency_scores/human_sal_{sal_metric_name}_{i}.npy')
         under_two,two_to_four,four_to_six,six_to_eight, above_eight = and_or_threshold(sal_scores)
-        np.save(f'data_splits/5_split/{i}_{sal_metric_name}_human_under_two', under_two)
-        np.save(f'data_splits/5_split/{i}_{sal_metric_name}_human_two_to_four', two_to_four)
-        np.save(f'data_splits/5_split/{i}_{sal_metric_name}_human_four_to_six', four_to_six)
-        np.save(f'data_splits/5_split/{i}_{sal_metric_name}_human_six_to_eight', six_to_eight)
-        np.save(f'data_splits/5_split/{i}_{sal_metric_name}_human_above_eight', above_eight)
+        np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_human_under_two', under_two)
+        np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_human_two_to_four', two_to_four)
+        np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_human_four_to_six', four_to_six)
+        np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_human_six_to_eight', six_to_eight)
+        np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_human_above_eight', above_eight)
 
         #plot graph -- for human map split
-        max_len_h = np.max([len(under_two),len(two_to_four),len(four_to_six), len(six_to_eight), len(above_eight)])
-        plt.bar(['<.2', '.2-.4','.4-.6','.6-.8', '>.8'],[len(under_two),len(two_to_four),len(four_to_six), len(six_to_eight), len(above_eight)])
+        sum = (len(under_two)+len(two_to_four)+len(four_to_six)+len(six_to_eight)+len(above_eight))
+        plt.bar(['<.2', '.2-.4','.4-.6','.6-.8', '>.8'],[len(under_two)/sum,len(two_to_four)/sum,len(four_to_six)/sum, len(six_to_eight)/sum, len(above_eight)/sum],label = 'Realistic Tampering (130)', color = (131/255.0,178/255.0,208/255.0,1.0))
         plt.xlabel("Saliency Of Manipulation Using Mean Recall")
-        plt.ylabel("Number Of Images")
-        graph_max_h =int(max_len_h + (max_len_h*.05))
-        plt.ylim(0,graph_max_h)
+        plt.ylabel("Proportion Of Images")
+        plt.ylim(0,.35)
         plt.title(f"Distribution Of {dataset_display}")
         plt.show()
-        plt.savefig(f'data_splits/5_split/histogram/splits_graph_{i}_{sal_metric_name}_human.png')
-        print(f'data_splits/5_split/histogram/splits_graph_{i}_{sal_metric_name}_human.png')
+        plt.savefig(f'./WIFS_ready/sal_groups/histogram/splits_graph_{i}_{sal_metric_name}_human.png')
+        print(f'./WIFS_ready/sal_groups/histogram/splits_graph_{i}_{sal_metric_name}_human.png')
         plt.clf()
 
 
     #combined map split
     under_two,two_to_four,four_to_six,six_to_eight,above_eight = and_or_threshold(scores)
-    np.save(f'data_splits/5_split/{i}_{sal_metric_name}_{split_metric_name}_under_two', under_two)
-    np.save(f'data_splits/5_split/{i}_{sal_metric_name}_{split_metric_name}_two_to_four', two_to_four)
-    np.save(f'data_splits/5_split/{i}_{sal_metric_name}_{split_metric_name}_four_to_six', four_to_six)
-    np.save(f'data_splits/5_split/{i}_{sal_metric_name}_{split_metric_name}_six_to_eight', six_to_eight)
-    np.save(f'data_splits/5_split/{i}_{sal_metric_name}_{split_metric_name}_above_eight', above_eight)
+    np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_{split_metric_name}_under_two', under_two)
+    np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_{split_metric_name}_two_to_four', two_to_four)
+    np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_{split_metric_name}_four_to_six', four_to_six)
+    np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_{split_metric_name}_six_to_eight', six_to_eight)
+    np.save(f'./WIFS_ready/sal_groups/{i}_{sal_metric_name}_{split_metric_name}_above_eight', above_eight)
 
     #plot graph -- for combined map split
-    plt.bar(['<.2', '.2-.4','.4-.6','.6-.8', '>.8'],[len(under_two),len(two_to_four),len(four_to_six), len(six_to_eight), len(above_eight)])
-    max_len = np.max([len(under_two),len(two_to_four),len(four_to_six), len(six_to_eight), len(above_eight)])
-    plt.xlabel("Saliency Of Manipulation Using Mean Recall")
-    plt.ylabel("Number Of Images")
-    graph_max =int(max_len + (max_len*.05))
-    plt.ylim(0,graph_max)
-    plt.title(f"Distribution Of {dataset_display}")
-    plt.show()
-    plt.savefig(f'data_splits/5_split/histogram/splits_graph_{i}_{sal_metric_name}.png')
-    print(f'data_splits/5_split/histogram/splits_graph_{i}_{sal_metric_name}.png')
-    plt.clf()
+    
+    sum = (len(under_two)+len(two_to_four)+len(four_to_six)+len(six_to_eight)+len(above_eight))
+    print(len(under_two))
+    print(len(two_to_four))
+    print(len(four_to_six))
+    print(len(six_to_eight))
+    print(len(above_eight))
+    lens.append([len(under_two)/sum,len(two_to_four)/sum,len(four_to_six)/sum, len(six_to_eight)/sum, len(above_eight)/sum])
+
+X = ['<.2', '.2-.4','.4-.6','.6-.8', '>.8']
+
+X_axis = np.arange(len(X))
+  
+plt.bar(X_axis - 0.2, lens[0], 0.2, label = 'Realistic Tampering (130)', color = (131/255.0,178/255.0,208/255.0,1.0))
+plt.bar(X_axis, lens[1], 0.2, label = 'MFC18 (1127)', color = (226/255.0, 116/255.0, 41/255.0,1.0) )
+plt.bar(X_axis + 0.2, lens[2], 0.2, label = 'IMD2020 (2007)', color = (149/255.0,218/255.0,182/255.0,1.0))
+
+plt.xticks(X_axis, X)
+    
+plt.xlabel("Saliency Of Manipulation Using Mean Recall")
+plt.ylabel("Proportion Of Images")
+plt.ylim(0,.55)
+
+plt.title(f"Distribution of Datasets")
+plt.legend()
+plt.show()
+plt.savefig(f'./WIFS_ready/sal_groups/histogram/splits_graph.png')
+print(f'./WIFS_ready/sal_groups/histogram/splits_graph.png')
+plt.clf()
