f95cc443c0f0685160519d8de5424e1bc231d306
Joshpk29 <44280379+Joshpk29@users.noreply.github.com>
2024-06-12 09:53:15 -0400

    Update for ICIP release

--- analysis_scripts/find_lengths.py
+++ analysis_scripts/find_lengths.py
@@ -9,6 +9,6 @@ thresh_metric_name = "and_or_threshold"
 
 for data in ['korus','MFC18_small', 'imd2020', 'imd2020_resize', 'MFC18_resize', 'korus_size_up', 'korus_SE', 'MFC18_SE','imd2020_SE']:
     for partition in ['under_two', 'two_to_four', 'four_to_six', 'six_to_eight', 'above_eight']:
-        values = np.load(f'./WIFS_ready/sal_groups/{data}_{sal_metric_name}_{thresh_metric_name}_{partition}.npy', allow_pickle = True)
+        values = np.load(f'./ICIP_2024/sal_groups/{data}_{sal_metric_name}_{thresh_metric_name}_{partition}.npy', allow_pickle = True)
         length = len(values)
         print(f'in dataset: {data} for partition: {partition} -- {length}')
